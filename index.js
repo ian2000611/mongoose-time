c0e32ab26546021e851bf0567b96a2c46b29ffe5
Ian Patterson <ian2000611@gmail.com>
2015-06-21 14:18:26 -0400

    updated to match plugin scheme from Mongoose 4.0.5

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
-
-/**
- * export `timestamps`
- */
-
-module.exports = timestamps;
-
-/**
- * sets up `created_at` n `updated_at`
- * prior to saving.
- *
- * example:
- *
- *      mySchema.plugin(timestamps());
- *
- * @param {Object} opts
- * @return {Function} Function
- */
-
-function timestamps(opts) {
-  return (function(schema){
+var debug = require('debug')('mongoose-time');
+module.exports = function timestamps(schema,opts) {
+    updated_at = opts.updated_at || 'updated_at';
+    created_at = opts.created_at || 'created_at';
+    updated_at_index = opts.updated_at_index || false;
+    created_at_index = opts.created_at_index || false;
+    debug('updated_at = ' + updated_at);
+    debug('created_at = ' + created_at);
+    debug("updated_at_index = " + updated_at_index);
+    debug("created_at_index = " + created_at_index);
     schema.add({ updated_at: Date, created_at: Date });
+    schema.path(updated_at).index(updated_at_index);
+    schema.path(created_at).index(created_at_index);
     schema.pre('save', function(next){
       var timestamp = new Date();
-      this.created_at = this.created_at || timestamp;
-      this.updated_at = timestamp;
+      this[created_at] = this[created_at] || timestamp;
+      this[updated_at] = timestamp;
+      debug (updated_at + '=' + this[updated_at] + '; '+ created_at +' = ' + this[created_at]);
       next();
     });
-  });
 };
